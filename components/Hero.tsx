a3c6d81e151391f4a334c3f3817e7315f2d2312f
Jun Hao Cheh <chehjh03@gmail.com>
2025-05-31 00:27:18 -0500

    Added auth, root routes

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image"; // image optimization
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1>
+          Your Career, Organized. Your Next Job, Closer!
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
